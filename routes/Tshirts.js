facd0cfa5b6e90d482fdda75e05fc53f147e2216
SaiUjwal <Venkatajaya953@gmail.com>
2023-11-28 15:34:35 -0600

    Update Tshirts.js

--- routes/Tshirts.js
+++ routes/Tshirts.js
@@ -1,10 +1,16 @@
 var express = require('express');
 const Tshirts_controllers= require('../controllers/Tshirts');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET Tshirts */
 router.get('/', Tshirts_controllers.Tshirts_view_all_Page );
 router.get('/detail', Tshirts_controllers.Tshirts_view_one_Page);
 router.get('/create', Tshirts_controllers.Tshirts_create_Page);
-router.get('/update', Tshirts_controllers.Tshirts_update_Page);
+router.get('/update',secured, Tshirts_controllers.Tshirts_update_Page);
 router.get('/delete', Tshirts_controllers.Tshirts_delete_Page);
 module.exports = router;
